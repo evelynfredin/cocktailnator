396c360e7b994f7af9b092244454ec61d598636d
Evelyn Fredin <evelynfredin@users.noreply.github.com>
2021-06-01 20:58:07 +0000

    Apply fixes from StyleCI

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,6 @@
 module.exports = {
-    mode: "jit",
-    purge: [
-        "./resources/**/*.blade.php",
-        "./resources/**/*.js",
-        "./resources/**/*.vue",
-    ],
+    mode: 'jit',
+    purge: ['./resources/**/*.blade.php', './resources/**/*.js', './resources/**/*.vue'],
     darkMode: false, // or 'media' or 'class'
     theme: {
         extend: {},
